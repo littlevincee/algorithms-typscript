a9e20e2542f332e925b6dbfefe39c780cf51f3ea
vinson.w <vinsonwong15@gmail.com>
2022-05-07 18:33:25 +0800

    using mocha and chi

--- src/__tests__/linkedList/linkedList.test.ts
+++
@@ -1,8 +0,0 @@
-import { LinkedList } from '../../algorithms/dataStructures/linkedList/linkedList'
-
-jest.mock('../../algorithms/dataStructures/linkedList/linkedList')
-
-it('it should be calling linkedList constructor to create an empty linked link', () => {
-  const linkedList = new LinkedList();
-  expect(LinkedList).toHaveBeenCalledTimes(1);
-})
